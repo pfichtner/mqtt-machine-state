9746c37ca68e1c7dc6700ab74c27874ae944c865
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2023-12-17 15:48:41 +0100

    path fixed (./binaries)

--- tests/approve-help.sh
+++ tests/approve-help.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the path to the binary
-BINARY_PATH="../mqttmachinestate"
+BINARY_PATH="../binaries/mqttmachinestate"
 
 # Set the path to the approved output file
 APPROVED_OUTPUT_FILE="approved_output.txt"
