d8e62de1afc0ca83c1a5debd6b1509337cc85b56
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2023-12-18 00:46:24 +0100

    migrated to docker-compose using toxiproxy

--- tests/itest.sh
+++ tests/itest.sh
@@ -2,35 +2,22 @@
 
 BINARY_PATH="../$1"
 
-# Function to generate a random free port
-get_random_free_port() {
-    local port
-    while true; do
-        port=$((RANDOM % (49151 - 1024) + 1024))
-        (echo > /dev/tcp/localhost/$port) >/dev/null 2>&1 || break
-    done
-    echo $port
+# Function to get the host port for a service
+get_host_port() {
+  SERVICE=$1
+  PORT=$2
+  PORT=$(docker-compose port $SERVICE $PORT | cut -d: -f2)
+  echo $PORT
 }
 
-# Function to start the MQTT broker container with a random free port on the host
-start_mqtt_broker() {
-    local random_port=$(get_random_free_port)
-    docker run --rm -d --name mqtt-broker -p $random_port:1883 -v $PWD/mosquitto/config/mosquitto.conf:/mosquitto/config/mosquitto.conf eclipse-mosquitto >/dev/null
-    echo $random_port
-}
+# Function to create a Toxiproxy listen/upstream pair
+create_toxiproxy_listen_upstream() {
+  PROXY_NAME=$1
+  LISTEN_PORT=$2
+  UPSTREAM_HOST=$3
+  UPSTREAM_PORT=$4
 
-# Function to start the binary with the given broker port and return its PID
-start_binary() {
-    local broker_port=$1
-    "$BINARY_PATH" -p $broker_port >/dev/null 2>&1 &
-    echo $!
-}
-
-# Function to wait for the MQTT broker to be ready
-wait_for_broker() {
-    until docker exec mqtt-broker mosquitto_pub -h localhost -p 1883 -t 'test' -m "test" &> /dev/null; do
-        sleep 1
-    done
+  docker exec $TOXIPROXY_CONTAINER_NAME /toxiproxy-cli create --listen 0.0.0.0:$LISTEN_PORT --upstream $UPSTREAM_HOST:$UPSTREAM_PORT $PROXY_NAME 
 }
 
 # Function to subscribe to a topic in the background and create a temporary file
@@ -38,7 +25,7 @@ subscribe_background() {
     local topic=$1
     local mqtt_output_file
     mqtt_output_file=$(mktemp)
-    docker exec mqtt-broker mosquitto_sub -h localhost -p 1883 -t $topic > "$mqtt_output_file" &
+    docker exec $MOSQUITTO_CONTAINER_NAME mosquitto_sub -h localhost -p 1883 -t $topic > "$mqtt_output_file" &
     echo "$mqtt_output_file"
 }
 
@@ -57,33 +44,61 @@ clear_message() {
     echo '' >"$mqtt_output_file"
 }
 
+# Function to start the binary with the given broker port and return its PID
+start_binary() {
+    local broker_port=$1
+    "$BINARY_PATH" -p $broker_port >/dev/null 2>&1 &
+    echo $!
+}
+
 # Function to stop and remove the MQTT broker container
 cleanup() {
-    docker rm -f mqtt-broker >/dev/null
+  # Stop and remove the containers
+  docker-compose down
+  rm $mqtt_output_file
 }
+
+# Start the services
+docker-compose up -d
+
+TOXIPROXY_CONTAINER_NAME=$(docker-compose ps -q toxiproxy)
+MOSQUITTO_CONTAINER_NAME=$(docker-compose ps -q mosquitto)
 
 # Set up trap to ensure cleanup on script termination or interruption
 trap cleanup EXIT
 
-# Start the MQTT broker container with a random free port on the host
-mqtt_broker_port=$(start_mqtt_broker)
+# Create Toxiproxy listen/upstream pair for MQTT broker
+create_toxiproxy_listen_upstream mqtt-broker 1884 mosquitto 1883
 
-# Wait for the MQTT broker to be ready
-wait_for_broker
+
+MOSQUITTO_DIRECT_PORT=$(get_host_port mosquitto 1883)
+MOSQUITTO_VIA_TOXIPROXY_PORT=$(get_host_port toxiproxy 1884)
+
 
 # Subscribe to the topic in the background and get the temporary file
 mqtt_output_file=$(subscribe_background "$(hostname)/status")
 sleep 3
 
 # Start the binary with the broker port and get its PID
-binary_pid=$(start_binary $mqtt_broker_port)
+binary_pid=$(start_binary $MOSQUITTO_VIA_TOXIPROXY_PORT)
 
 # Wait for the "online" message
 check_message "online" "$mqtt_output_file" 10  # Adjust the timeout as needed
+clear_message "$mqtt_output_file"
+
+# ADD toxic
+docker exec $TOXIPROXY_CONTAINER_NAME /toxiproxy-cli toxic add -t timeout -n timeout_downstream -a timeout=1 mqtt-broker
+check_message "offline" "$mqtt_output_file" 30  # Adjust the timeout as needed
+clear_message "$mqtt_output_file"
+
+# REMOVE toxic
+docker exec $TOXIPROXY_CONTAINER_NAME /toxiproxy-cli toxic remove -n timeout_downstream mqtt-broker
+check_message "online" "$mqtt_output_file" 20  # Adjust the timeout as needed
 clear_message "$mqtt_output_file"
 
 # Kill the binary and wait for the "offline" message
 kill $binary_pid
 check_message "offline" "$mqtt_output_file" 10  # Adjust the timeout as needed
 clear_message "$mqtt_output_file"
+
 
