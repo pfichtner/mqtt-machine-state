82703d6e67e833079b17d6b72620a30c3f56dd1a
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2023-12-18 14:47:46 +0100

    scrub the binary-name as well

--- tests/approve-help.sh
+++ tests/approve-help.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Set the path to the binary
-BINARY_PATH="../$1"
+BINARY_NAME="$1"
+BINARY_PATH="../$BINARY_NAME"
 
 # Set the path to the approved output file
 APPROVED_OUTPUT_FILE="approved_output.txt"
@@ -9,19 +10,25 @@ APPROVED_OUTPUT_FILE="approved_output.txt"
 # Set the path to the actual output file
 ACTUAL_OUTPUT_FILE="actual_output.txt"
 
-# Scrub placeholder for hostname
-SCRUB_PLACEHOLDER="\$\$\$SCRUBBED_HOSTNAME\$\$\$"
+# Scrub placeholders for hostname and binary name
+SCRUB_HOSTNAME_PLACEHOLDER="\$\$\$SCRUBBED_HOSTNAME\$\$\$"
+SCRUB_BINARY_NAME_PLACEHOLDER="\$\$\$SCRUBBED_BINARY_NAME\$\$\$"
 
 # Check if the script is invoked with the "--approve" option
-if [ "$1" = "--approve" ]; then
-  # Run the binary with the "--help" argument, replace the hostname, and save both stdout and stderr to the approved file
-  { "$BINARY_PATH" --help 2>&1 | sed "s/$(hostname)/$SCRUB_PLACEHOLDER/g"; } > "$APPROVED_OUTPUT_FILE"
+if [ "$BINARY_NAME" = "--approve" ]; then
+  echo "Error: Missing binary name. Usage: $0 <binary_name>"
+  exit 1
+fi
+
+if [ "$2" = "--approve" ]; then
+  # Run the binary with the "--help" argument, replace the hostname and binary name, and save both stdout and stderr to the approved file
+  { "$BINARY_PATH" --help 2>&1 | sed -e "s/$(hostname)/$SCRUB_HOSTNAME_PLACEHOLDER/g" -e "s/$BINARY_NAME/$SCRUB_BINARY_NAME_PLACEHOLDER/g"; } > "$APPROVED_OUTPUT_FILE"
   echo "Approved output updated."
   exit 0
 fi
 
 # Run the binary with the "--help" argument and capture both stdout and stderr
-{ "$BINARY_PATH" --help 2>&1 | sed "s/$(hostname)/$SCRUB_PLACEHOLDER/g"; } > "$ACTUAL_OUTPUT_FILE"
+{ "$BINARY_PATH" --help 2>&1 | sed -e "s/$(hostname)/$SCRUB_HOSTNAME_PLACEHOLDER/g" -e "s/$BINARY_NAME/$SCRUB_BINARY_NAME_PLACEHOLDER/g"; } > "$ACTUAL_OUTPUT_FILE"
 
 # Compare the current output with the approved output
 if diff -w "$APPROVED_OUTPUT_FILE" "$ACTUAL_OUTPUT_FILE" > /dev/null; then
