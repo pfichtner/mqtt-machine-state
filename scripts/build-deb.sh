0f77d9d742e5f951f11d8065442490ad5936688c
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2025-12-15 15:08:31 +0100

    initial version of building dep packages

---
+++ scripts/build-deb.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+set -e
+
+PKGROOT="deb-pkg"
+PKGNAME="mqttmachinestate"
+VERSION="${1:-0.0.0}"
+ARCH="${2:-amd64}"
+
+rm -rf "$PKGROOT"
+mkdir -p "$PKGROOT/DEBIAN" "$PKGROOT/usr/bin" "$PKGROOT/etc" "$PKGROOT/lib/systemd/system" "$PKGROOT/usr/share/doc/$PKGNAME"
+
+# Copy binary
+install -m 0755 binaries/mqttmachinestate "$PKGROOT/usr/bin/"
+
+# Copy default config
+cp debian/mqttmachinestate.conf.tmpl "$PKGROOT/etc/mqttmachinestate.conf"
+chmod 644 "$PKGROOT/etc/mqttmachinestate.conf"
+chown root:root "$PKGROOT/etc/mqttmachinestate.conf"
+
+# Copy systemd service
+cp debian/mqtt-machine.service.tmpl "$PKGROOT/lib/systemd/system/mqtt-machine.service"
+chmod 644 "$PKGROOT/lib/systemd/system/mqtt-machine.service"
+chown root:root "$PKGROOT/lib/systemd/system/mqtt-machine.service"
+
+# Copy README (optional)
+[ -f README.md ] && cp README.md "$PKGROOT/usr/share/doc/$PKGNAME/"
+
+# Render control
+envsubst < debian/control.tmpl > "$PKGROOT/DEBIAN/control"
+
+# Copy maintainer scripts
+cp debian/postinst.tmpl "$PKGROOT/DEBIAN/postinst"
+cp debian/prerm.tmpl "$PKGROOT/DEBIAN/prerm"
+chmod 755 "$PKGROOT/DEBIAN/postinst" "$PKGROOT/DEBIAN/prerm"
+
+# Build the .deb
+dpkg-deb --build "$PKGROOT"
+
+# Move to artifacts
+mkdir -p publish-artifacts
+mv "${PKGROOT}.deb" "publish-artifacts/${PKGNAME}_${VERSION}_${ARCH}.deb"
+
